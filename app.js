092f15ab4f332635aea0072e878168132aa46220
v2mplay <layssa.barros@sptech.school>
2024-09-07 14:01:42 -0300

    Projeto pré-concluido

--- app.js
+++
@@ -1,8 +0,0 @@
-enviarButton.addEventListener('click', () => {
-    let acertos = 0;
-
-    resultado.textContent = `Você acertou ${acertos} perguntas`;
-    resultado.style.display = 'block';
-});
-
-console.log(acertos);
--- dados.js
+++
@@ -1,9 +0,0 @@
-const painelQuiz = document.getElementById('painelQuiz');
-const enviarButton = document.getElementById('enviar');
-const resultado = document.getElementById('resultado');
-
-const respostasCorretas = [
-    'Rick Grimes', 'Policial', 'Verdadeiro',
-    'Beth Greene', 'Verdadeiro','The Day Will Come When You Won t Be',
-    'Nenhuma'
-];
