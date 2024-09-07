aa45eb79aea32e3172cbb8c1be32069aebe2fc07
v2mplay <layssa.barros@sptech.school>
2024-09-07 01:31:09 -0300

    Quiz implementado

---
+++ dados.js
@@ -0,0 +1,9 @@
+const painelQuiz = document.getElementById('painelQuiz');
+const enviarButton = document.getElementById('enviar');
+const resultado = document.getElementById('resultado');
+
+const respostasCorretas = [
+    'Rick Grimes', 'Policial', 'Verdadeiro',
+    'Beth Greene', 'Verdadeiro','The Day Will Come When You Won t Be',
+    'Nenhuma'
+];
